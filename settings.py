9864f6265a35c02e3bff30cc6c0de439127f8a6d
dalbrechtNL <d.albrecht@maastrichtuniversity.nl>
2022-03-03 16:58:21 +0100

    Initial commit

---
+++ settings.py
@@ -0,0 +1,47 @@
+from os import environ
+
+SESSION_CONFIGS = [
+    dict(
+        name='double_auction',
+        display_name="Double auction market",
+        app_sequence=['double_auction'],
+        num_demo_participants=4,
+    ),
+]
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+
+SESSION_CONFIG_DEFAULTS = dict(real_world_currency_per_point=1.00, participation_fee=0.00, doc="")
+
+# ISO-639 code
+# for example: de, fr, ja, ko, zh-hans
+LANGUAGE_CODE = 'en'
+
+# e.g. EUR, GBP, CNY, JPY
+REAL_WORLD_CURRENCY_CODE = 'USD'
+USE_POINTS = True
+
+ADMIN_USERNAME = 'admin'
+# for security, best to set admin password in an environment variable
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+
+DEMO_PAGE_INTRO_HTML = """ """
+DEMO_PAGE_TITLE = "Economy Game 2.0"
+
+SECRET_KEY = '4387860144726'
+
+# if an app is included in SESSION_CONFIGS, you don't need to list it here
+INSTALLED_APPS = ['otree']
+
+PARTICIPANT_FIELDS = [
+    #'example1',
+    #'example2',
+]
+
+SESSION_FIELDS = [
+    #'example1',
+    #'example2',
+]
