5e2d393581d0e72ae8c08c87b2102c5404a73750
dalbrechtNL <d.albrecht@maastrichtuniversity.nl>
2022-04-29 11:03:30 +0200

    Solving format issues of offer and trading history and bids/asks

--- double_auction/__init__.py
+++ double_auction/__init__.py
@@ -102,7 +102,6 @@ def creating_session(subsession: Subsession):
         participant.time_needed = 0
         participant.previous_timestamp = time.time()
         participant.current_timestamp = time.time()
-        participant.refresh_counter = 0
         participant.error = ""
 
 
@@ -286,7 +285,7 @@ def live_method(player: Player, data):
                     for x in buyer.participant.offer_times:
                         buyer.participant.offer_history.append({"offer": str('{:.2f}'.format(round(x[0]))) + " " +
                                                                          currency_unit,
-                                                                 "offer_time": datetime.fromtimestamp(x[1]).ctime()})
+                                                                "offer_time": datetime.fromtimestamp(x[1]).ctime()})
                     seller.participant.offer_history = []  # Empty offer history before recreating based on most recent info
                     for x in seller.participant.offer_times:
                         seller.participant.offer_history.append({"offer": str('{:.2f}'.format(round(x[0]))) + " " +
@@ -339,22 +338,23 @@ def live_method(player: Player, data):
                 player.participant.marginal_evaluation = marginal_consumption_utility(player.participant.time_needed)
             else:
                 player.participant.marginal_evaluation = marginal_production_costs(player.participant.time_needed)
-            # Update refresh counter
-            if player.participant.refresh_counter == 10:
-                player.participant.refresh_counter = 0
-            else:
-                player.participant.refresh_counter += 1
 
     # Create lists of all asks/bids by all sellers/buyers
     raw_bids = [[i[0] for i in p.participant.offer_times] for p in buyers]  # Collect bids from all buyers
     raw_bidders = [[p.id_in_group for i in p.participant.offer_times] for p in buyers]  # Collect bidders
     bids = flatten(raw_bids)  # Unnest list of bids
     bidders = flatten(raw_bidders)  # Unnest list of bidders
-    bids_index = [i for i in range(len(bids))]
-    # Dictionary of bid index and bid amount
-    bids_dict = dict(zip(bids_index, [str('{:.2f}'.format(round(x, 2))) for x in bids]))
-    bidders_dict = dict(zip(bids_index, bidders))  # Dictionary of bid index and bidder.id_in_group
-    # bidders_dict = dict(zip(bids_index, [bool(x == player.id_in_group) for x in bidders]))
+    # Dictionary of bid amount and bidder
+    bid_keys = ["bid" for i in range(len(bids))]
+    bidder_keys = ["bidder" for i in range(len(bidders))]
+    overall_bids_dict_list = [[bid_keys[i], bids[i], bidder_keys[i], bidders[i]] for i in range(len(bids))]
+    overall_bids = [
+            {
+                overall_bids_dict_list[i][0]: str('{:.2f}'.format(round(overall_bids_dict_list[i][1], 2))),
+                overall_bids_dict_list[i][2]: overall_bids_dict_list[i][3]
+            }
+            for i in range(len(overall_bids_dict_list))
+        ]
     bids.sort(reverse=True)
 
     # Collect asks from all sellers
@@ -362,10 +362,17 @@ def live_method(player: Player, data):
     raw_askers = [[p.id_in_group for i in p.participant.offer_times] for p in sellers]  # Collect sellers
     asks = flatten(raw_asks)  # Unnest list
     askers = flatten(raw_askers)  # Unnest list of sellers
-    asks_index = [i for i in range(len(asks))]
-    # Dictionary of ask index and ask amount
-    asks_dict = dict(zip(asks_index, [str('{:.2f}'.format(round(x, 2))) for x in asks]))
-    askers_dict = dict(zip(asks_index, askers))  # Dictionary of bid index and bidder.id_in_group
+    # Dictionary of ask amount and asker
+    ask_keys = ["ask" for i in range(len(asks))]
+    asker_keys = ["asker" for i in range(len(askers))]
+    overall_asks_dict_list = [[ask_keys[i], asks[i], asker_keys[i], askers[i]] for i in range(len(asks))]
+    overall_asks = [
+            {
+                overall_asks_dict_list[i][0]: str('{:.2f}'.format(round(overall_asks_dict_list[i][1], 2))),
+                overall_asks_dict_list[i][2]: overall_asks_dict_list[i][3]
+            }
+            for i in range(len(overall_asks_dict_list))
+        ]
     asks.sort(reverse=False)
 
     # Create charts
@@ -376,11 +383,8 @@ def live_method(player: Player, data):
                 player.session.config['currency_unit']),
             current_offer_time=datetime.fromtimestamp(p.current_offer_time).ctime(),
             balance=str('{:.2f}'.format(round(p.balance, 2))) + " " + str(player.session.config['currency_unit']),
-            bids=[str('{:.2f}'.format(round(x, 2))) for x in bids],
-            # Create a dictionaries of offers with an offer index, offer amount and player.id_in_group
-            bids_dict=json.dumps(dict(bid=bids_dict, bidder=bidders_dict)),
-            asks=[str('{:.2f}'.format(round(x, 2))) for x in asks],
-            asks_dict=json.dumps(dict(ask=asks_dict, asker=askers_dict)),
+            bids=overall_bids,  # json.dumps(overall_bids_dict),
+            asks=overall_asks,  # json.dumps(overall_asks_dict),
             highcharts_series=highcharts_series,
             cost_chart_series=cost_chart_series,
             chart_point=[[p.participant.time_needed, p.participant.marginal_evaluation]],
@@ -388,12 +392,11 @@ def live_method(player: Player, data):
             news=news,
             offers=[str('{:.2f}'.format(round(i[0], 2))) for i in p.participant.offer_times],
             offer_times=[datetime.fromtimestamp(tup[1]).ctime() for tup in p.participant.offer_times],
-            offer_history=p.participant.offer_history,
+            offer_history=p.participant.offer_history,  # json.dumps(dict(offers=p.participant.offer_history)),
             time_needed=p.participant.time_needed,
             marginal_evaluation=str('{:.2f}'.format(round(p.participant.marginal_evaluation, 2))) + " " + str(
                 player.session.config['currency_unit']),
-            trading_history=p.participant.trading_history,
-            refresh_counter=p.participant.refresh_counter,
+            trading_history=p.participant.trading_history,  # json.dumps(dict(trades=p.participant.trading_history)),
             error=p.participant.error,
         )
         for p in players
